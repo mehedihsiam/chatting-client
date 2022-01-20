3401c64d18e2e2acb93d2aaa86c1c6594fa07f77
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2022-01-20 21:00:08 +0600

    update

--- src/Components/LeftMenu/LeftMenu.js
+++ src/Components/LeftMenu/LeftMenu.js
@@ -1,10 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser, faCogs, faSignOutAlt, faTh } from '@fortawesome/free-solid-svg-icons';
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
 import React from 'react';
 import './LeftMenu.css'
+
+
 
 const LeftMenu = () => {
     return (
         <div className='left-menu'>
-            menu
+            <div className="top">
+                <FontAwesomeIcon icon={faTh} />
+            </div>
+
+            <div className="menu">
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faHouseUser} />
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faCommentDots} />
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faCogs} />
+                    </li>
+                </ul>
+            </div>
+
+            <div className="logout">
+                <FontAwesomeIcon icon={faSignOutAlt} />
+            </div>
         </div>
     );
 };
